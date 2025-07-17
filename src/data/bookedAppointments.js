4e4e62648603479049d7cb73b4efeba28fc4ac98
Hugh <hughfeehan22@gmail.com>
2025-07-17 15:31:48 +0100

    Made changes to bookedAppointments, must do setBookings function

--- src/data/bookedAppointments.js
+++ src/data/bookedAppointments.js
@@ -1,19 +1,25 @@
-export function getBookedAppointments() {
-  const bookedAppointments = [
-    {
-      title: 'Dr. Murphy - Baby C',
-      start: new Date(2025, 10, 12, 10, 0),
-      end: new Date(2025, 10, 12, 11, 0),
-      type: 'booked'
-    },
-    {
-      title: 'Dr. OConnell - Baby D',
-      start: new Date(2025, 10, 14, 9, 30),
-      end: new Date(2025, 10, 14, 10, 30),
-      type: 'booked'
-    },
-  ];
+let bookedAppointments = [ // Set list with appointment objects
+  {
+    title: 'Dr. Murphy - Baby C',
+    start: new Date(2025, 10, 12, 10, 0),
+    end: new Date(2025, 10, 12, 11, 0),
+    type: 'booked'
+  },
+  {
+    title: 'Dr. OConnell - Baby D',
+    start: new Date(2025, 10, 14, 9, 30),
+    end: new Date(2025, 10, 14, 10, 30),
+    type: 'booked'
+  },
+];
 
-return bookedAppointments
+// Function to return list of appointment objects
+export function getBookedAppointments(){
+  return bookedAppointments
+  }
 
+// Funtion to add appointments
+export function addBookedAppointment(appointment) {
+  bookedAppointments.push(appointment)
 }
+
--- src/data/eventPropGetter.js
+++ src/data/eventPropGetter.js
@@ -9,7 +9,7 @@ export function eventPropGetter(event)  { // Event property getter function. See
       border: 'none',
       padding: '2px',
     };
-  } else if (event.type == 'window') {
+  } else if (event.type === 'window') {
     style = {
       backgroundColor: '#0d6efd', // blue for visit windows
       color: '#fff',
