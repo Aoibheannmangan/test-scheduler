4affb6ba07cf20dec36efe77be625207147d39fe
Hugh <hughfeehan22@gmail.com>
2025-08-06 14:43:19 +0100

    Changed config for transforming new JSX elements when testing

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
 /** @type {import('jest').Config} */
 module.exports = {
   verbose: true,
-  transform: {
-    "^.+\\.jsx?$": "babel-jest"
-  },
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "<rootDir>/styleMock.js"
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
-
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  // Enable modern JSX transform
+  transform: { "^.+\\.(js|jsx|ts|tsx)$": [ "babel-jest", { presets: [ [ "next/babel", { "preset-react": { runtime: "automatic",  }, }, ], ], }, ], },
 };
