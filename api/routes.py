b9120e880f5bdb5404136f2f92fb26b9f93e5364
Hugh <hughfeehan22@gmail.com>
2025-08-26 13:10:36 +0100

    All data

--- api/routes.py
+++ api/routes.py
@@ -5,21 +5,10 @@ from config import REDCAP_API_URL, API_TOKEN
 def get_data():
     # Define the payload with the required parameters
     payload = {
-        'token': API_TOKEN,  # Use the API token from your config
-        'content': 'record',
-        'action': 'export',
-        'format': 'json',
-        'type': 'flat',
-        'csvDelimiter': '',
-        'records[0]': '1',
-        'fields[0]': 'nicu_dob',
-        'forms[0]': 'neonatal_clinical_information',
-        'rawOrLabel': 'raw',
-        'rawOrLabelHeaders': 'raw',
-        'exportCheckboxLabel': 'false',
-        'exportSurveyFields': 'false',
-        'exportDataAccessGroups': 'false',
-        'returnFormat': 'json'
+    'token': API_TOKEN,
+    'content': 'record',
+    'format': 'json',
+    'type': 'flat'
     }
     
     # Make the POST request to the REDCap API
@@ -30,8 +19,6 @@ def get_data():
         data = response.json()  # Parse the JSON response
         return jsonify(data)  # Return the data as a JSON response
 
-    elif response.status_code != 200:
-        print('Error:', response.json())  # Print the error message
         
     else:
         return jsonify({'error': 'Failed to fetch data', 'status_code': response.status_code}), response.status_code
