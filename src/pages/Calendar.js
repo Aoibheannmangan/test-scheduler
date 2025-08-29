33cbcc1d16fcf304e506e2fc2a2260f6a8afefbe
Aoibheann Mangan <amangan@ucc.ie>
2025-08-29 11:16:49 +0100

    made text a little bigger cause i found it hard to read

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -752,11 +752,11 @@ const MyCalendar = () => {
             event: ({ event }) => (
               <div>
                 {event.title}
-                <div style={{ fontSize: 12 }}>
+                <div style={{ fontSize: 14 }}>
                   <strong>{new Date(event.start).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</strong> -
                   <strong>{new Date(event.end).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}</strong>
                 </div>
-                <div style={{ fontSize: 10 }}>
+                <div style={{ fontSize: 12 }}>
                   <strong>{event.notes}</strong>
                 </div>{" "}
                 {/* Show description here */}
