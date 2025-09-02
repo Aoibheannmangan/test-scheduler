1c90d4986d5ce12d41c95abe53d645be2d9b7e36
Aoibheann Mangan <aoibheannmangan@gmail.com>
2025-09-02 09:22:40 +0100

    Update config.py (Removed API Token)

--- config.py
+++ config.py
@@ -3,4 +3,4 @@
 import os
 
 REDCAP_API_URL = os.getenv('REDCAP_API_URL', 'https://redcap.ucc.ie/api/')
-API_TOKEN = os.getenv('API_TOKEN', '866DC544C7E9B61B5F323F999235D6BC') # Change API token if needed
+API_TOKEN = os.getenv('API_TOKEN', ' ') # Change API token if needed
