fe4a373f7ec8dcdacfb91d6096e054cd24cf382b
Aoibheann <aoibheannmangan@gmail.com>
2025-10-28 16:45:35 +0000

    small error became a big error

--- src/components/ClickableCell.js
+++ src/components/ClickableCell.js
@@ -6,7 +6,9 @@
 import moment from 'moment';
 
 const ClickableDateCell = ({ children, value, onSelectSlot, blockedDates }) => {
-  const isBlocked = blockedDates.some((evt) => moment(value).isSame(evt.start, "day"));
+  const safeBlockedDates = Array.isArray(blockedDates) ? blockedDates : [];
+  
+  const isBlocked = safeBlockedDates.some((evt) => moment(value).isSame(evt.start, "day"));
 
   const handleClick = () => {
     if (isBlocked) return;
@@ -20,16 +22,18 @@ const ClickableDateCell = ({ children, value, onSelectSlot, blockedDates }) => {
   const cellStyle = {
     cursor: isBlocked ? 'not-allowed' : 'pointer',
     height: '100%',
+    weight: '100%',
     backgroundColor: isBlocked ? '#ff0000ff' : 'transparent',
     borderRadius: "6px",
     opacity: isBlocked ? 0.6 : 1,
+    display: 'flex',
   }
 
   return (
     <div
     // Calls handleClick when clicked  
       onClick={handleClick}
-      style={{ cursor: 'pointer', height: '100%' }}
+      style={{ cellStyle }}
     >
      {/* Displays content inside date*/}
       {children}
