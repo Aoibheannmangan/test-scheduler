741ae47aeb0bd8f2b8b6e2e19fec1954a3416619
Aoibheann <aoibheannmangan@gmail.com>
2025-10-28 12:56:19 +0000

    Maybe months in order now?

--- src/pages/Forecast.js
+++ src/pages/Forecast.js
@@ -23,7 +23,15 @@ const Forecast = () => {
                 <p>There are currently no appointments booked</p>
             ) : (
                 <ul>
-                    {Object.entries(monthlyCounts).map(([monthYear, count]) => (
+                    {Object.entries(monthlyCounts)
+                        .sort(([a], [b]) => {
+                            const [monthA, yearA] = a.split('');
+                            const [monthB, yearB] = b.split('');
+                            const dateA = new Date(`${monthA} 1, ${yearA}`);
+                            const dateB = new Date(`${monthB} 1, ${yearB}`);
+                            return dateA - dateB;
+                        })
+                        .map(([monthYear, count]) => (
                         <li key={monthYear}>
                             {monthYear}: {count} appointment{count !== 1 ? 's' : ''}
                         </li>
