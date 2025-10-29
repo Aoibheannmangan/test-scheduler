e76e58facc4c317dca64702f255f775d5a260e9e
Hugh <hughfeehan22@gmail.com>
2025-10-29 08:11:38 +0000

    Switch from Drizzle to Flask SQLAlchemy for simplicity

--- server/drizzle/db/client.js
+++
@@ -1,8 +0,0 @@
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import dotenv from "dotenv";
-dotenv.config();
-
-const DB_FILE = process.env.DB_FILE || "./data/scheduler.db";
-const raw = new Database(DB_FILE);
-export const db = drizzle(raw);
--- server/drizzle/db/database.js
+++
@@ -1,8 +0,0 @@
-// File for db using Drizzle ORM
-
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
-import * as schema from "./schema";
-
-const db = new Database("./dev.db"); // adjust path as needed
-export const client = drizzle(db);
