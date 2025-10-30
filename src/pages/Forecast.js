7ede9868465819d0875d6b50f0f8da6bc148bd7c
Aoibheann <aoibheannmangan@gmail.com>
2025-10-30 10:05:15 +0000

    windows forecast

--- src/pages/Forecast.js
+++ src/pages/Forecast.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState} from 'react'; 
-import { getAppointmentsPerMonth }  from '../hooks/forecast';
+import { getAppointmentsPerMonth, getWindowsPerMonth }  from '../hooks/forecast';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -9,20 +9,33 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TablePagination from '@mui/material/TablePagination';
 import { min } from 'moment';
+import Tab from '@mui/material/Tab';
 
 const columns = [
     {id: "monthYear", label: "Month", minWidth: 170},
     {id: "count", label: "Number of Appointments", minWidth: 100, align: "right" }
 ];
 
+const wincolumns = [
+  {id: "windowMonthYear", label: "Month", minWidth: 170},
+  {id: "windowCount", label: "Number of Visit Windows", minWidth: 100, align: "right" }
+]
+
 function createData(monthYear, count){
     return {monthYear, count};
 }
 
+function createWinData(windowMonthYear, windowCount){
+  return {windowMonthYear, windowCount};
+}
+
 const Forecast = () => { 
     const [bookedEvents, setBookedEvents] = useState([]);
+    const [studyWindows, setStudyWindows] = useState([]);
     const [monthlyCounts, setMonthlyCounts] = useState([]);
+    const [windowCounts, setWindowCounts] = useState([]);
     const [rows, setRows] = useState([]);
+    const [windowRows, setWindowRows] = useState([]);
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
 
@@ -43,6 +56,11 @@ const Forecast = () => {
     }, [bookedEvents]);
 
     useEffect(() => {
+        const counts = getWindowsPerMonth(studyWindows);
+        setWindowCounts(counts);
+    }, [studyWindows]);
+
+    useEffect(() => {
         const sortedEntries = Object.entries(monthlyCounts).sort(([a], [b]) => {
             const [monthA, yearA] = a.split(' ');
             const [monthB, yearB] = b.split(' ');
@@ -55,11 +73,26 @@ const Forecast = () => {
         setRows(dataRows);
     }, [monthlyCounts]);
 
+    useEffect(() => {
+      const sortedWindowEntries = Object.entries(windowCounts).sort(([a], [b]) => {
+        const [monthA, yearA] = a.split(' ');
+        const [monthB, yearB] = b.split(' ');
+        const dateA = new Date(`${monthA} 1, ${yearA}`);
+        const dateB = new Date(`${monthB} 1, ${yearB}`);
+        return dateA - dateB;
+      });
+
+      const windowDataRows = sortedWindowEntries.map(([windowMonthYear, windowCount]) => createWinData(windowMonthYear, windowCount));
+        setWindowRows(windowDataRows);
+      }, [windowCounts]);
+
     const handleChangePage = (_event, newPage) => setPage(newPage);
     const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(+event.target.value);
         setPage(0);
     };
+
+    
 
     return ( 
       <div style={{ padding: "1.5rem" }}>
@@ -116,6 +149,56 @@ const Forecast = () => {
           </Paper>
         )}
         <h1>Visit Window Forecast</h1>
+        {windowRows.length === 0 ? (
+          <p>There are no visit windows currently</p>
+        ) : (
+          <Paper sx={{ width: "100%", overflow: "hidden" }}>
+            <TableContainer sx={{ maxHeight: 440 }}>
+              <Table stickyHeader aria-label="forecast table">
+                <TableHead>
+                  <TableRow>
+                    {wincolumns.map((column) => (
+                      <TableCell
+                        key={column.id}
+                        align={column.align}
+                        style={{ minWidth: column.minWidth }}
+                      >
+                        {column.label}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {windowRows
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => (
+                      <TableRow hover TabIndex={-1} key={row.windowMonthYear}>
+                        {wincolumns.map((column) => {
+                          const value = row[column.id];
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              {column.format && typeof value === "number"
+                                ? column.format(value)
+                                : value}
+                            </TableCell>
+                          );
+                        })}
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination 
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={windowRows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </Paper>
+        )}
       </div>
       
   );
