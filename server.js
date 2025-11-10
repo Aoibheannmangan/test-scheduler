31323223022f96ebc18fa0c748ee2c0171d65879
Aoibheann <aoibheannmangan@gmail.com>
2025-11-10 16:06:10 +0000

    another change

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/api/some-endpoint', (req,res) => {
+    res.json({message: "Test"});
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
