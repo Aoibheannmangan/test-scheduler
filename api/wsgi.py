726f418baac537765c29f863e0ca5cbf27efde34
Aoibheann <aoibheannmangan@gmail.com>
2025-11-13 16:10:27 +0000

    Fix imports for Render deployment

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,6 +1,6 @@
 from api.scheduler_api import create_app
-
+from api.scheduler_api.models import db
 app = create_app()
+with app.app_context():
+    db.create_all()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
