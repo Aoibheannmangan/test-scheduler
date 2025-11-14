aebf85483cb5512522a6ef3ce74ed329814529d8
Aoibheann <aoibheannmangan@gmail.com>
2025-11-14 10:54:41 +0000

    changing links

--- src/pages/login.js
+++ src/pages/login.js
@@ -53,7 +53,8 @@ const LogIn = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await axios.post("http://localhost:5000/api/login", {
+      const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000"
+      const response = await axios.post(`${API_URL}/api/login`, {
         email,
         password,
       });
