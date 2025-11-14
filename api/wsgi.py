2348e2a5b6c4ab306080a726d6b02f1f0891a3a5
Aoibheann <aoibheannmangan@gmail.com>
2025-11-14 11:33:36 +0000

    small change

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,10 +1,9 @@
 from api.scheduler_api import create_app
 from api.scheduler_api.models import db
+import os
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port)
