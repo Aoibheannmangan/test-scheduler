251194a95927ebfe0d2032aa369710d377d934ec
Aoibheann <aoibheannmangan@gmail.com>
2025-11-19 13:38:08 +0000

    fix?

--- api/scheduler_api/routes.py
+++ api/scheduler_api/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, current_app, request
 from flask_cors import CORS
 import requests
-from config import REDCAP_API_URL, API_TOKEN
+from ..config import REDCAP_API_URL, API_TOKEN
 import logging
 import os
 from .extensions import db
