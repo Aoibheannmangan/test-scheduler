b955e6748e32bda06906751d2f22a58d735eba75
Hugh <hughfeehan22@gmail.com>
2025-12-04 13:22:12 +0000

    Styled footer + formatted code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,30 +5,29 @@ import "./Footer.css";
 // Footer placed at the bottom of all pages
 const Footer = () => {
   return (
-    <div className="Footer">
-      <hr />
-      <div className="container-pair">
-        <div className="box">
-          <h2>Hosted at:</h2>
-          <img src={uccLogo} alt="UccLogo" />
-        </div>
-        <div className="box">
-          <h2>Contact:</h2>
-          <p className="contact-info">
-            Email: infant@ucc.ie
-            <br />
-            Tel: +353(0)21 4205082
-            <br />
-            <br />
-            Paediatric Academic Unit, <br />
-            Cork University Hospital, <br />
-            Wilton, Cork
-            <br />
-            Ireland
-          </p>
-        </div>
-      </div>
-    </div>
+		<div className="Footer">
+			<div className="container-pair">
+				<div className="box">
+					<h2>Hosted at:</h2>
+					<img src={uccLogo} alt="UccLogo" />
+				</div>
+				<div className="box">
+					<h2>Contact:</h2>
+					<p className="contact-info">
+						Email: infant@ucc.ie
+						<br />
+						Tel: +353(0)21 4205082
+						<br />
+						<br />
+						Paediatric Academic Unit, <br />
+						Cork University Hospital, <br />
+						Wilton, Cork
+						<br />
+						Ireland
+					</p>
+				</div>
+			</div>
+		</div>
   );
 };
 
